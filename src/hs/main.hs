371f89d5e02abe2ec7a06555a7fdd81624b8cd0b
Ksawery Chodyniecki <ksawery.chodyniecki@gmail.com>
2022-05-03 12:18:55 +0200

    readded help and other messages

--- src/hs/main.hs
+++ src/hs/main.hs
@@ -14,6 +14,7 @@ data Path = Path {
     } deriving (Show)
 
 data State = State {
+    message :: [String],
     i_am_at :: String
     } deriving (Show)
 
@@ -39,32 +40,36 @@ paths =
 
 go state direction = do
     case (List.find (\(x) -> from x == i_am_at state && by x == direction) paths) of
-        Nothing -> state
-        Just(path) -> State (to path)
+        Nothing -> state { message = ["You can't go that way!"] }
+        Just(path) -> state { i_am_at = to path }
     -- look
 
 describeIntroduction = [
-    "Daily life in the alpine hotel was disrupted by the barking of the Promyczek. The wife, which came after that to the room, saw her husband on the floor. She started screaming and woke up everyone who stayed in the hotel. The hotel owner immediately calls you to solve that riddle. Who is the murderer? You don't know. Yet..."
+        "",
+        "Daily life in the alpine hotel was disrupted by the barking of the Promyczek. The wife, which came after that to the room, saw her husband on the floor. She started screaming and woke up everyone who stayed in the hotel. The hotel owner immediately calls you to solve that riddle. Who is the murderer? You don't know. Yet...",
+        ""
     ]
 
 describeHelp = [
     "Available commands are:",
-    "",
     "help          -- to see these instructions.",
-    "w",
-    "d",
-    "a",
-    "s",
+    "quit          -- to end the game and quit.",
+    "w             -- go up / north.",
+    "d             -- go right / east.",
+    "a             -- go left / west.",
+    "s             -- go down / south.",
     ""
     ]
 
 printLines :: [String] -> IO ()
 printLines xs = putStr (unlines xs)
 
--- help :: State -> IO State
--- help state = do
---     printLines describeHelp
---     state
+printState :: State -> IO ()
+printState state = do
+    putStr (unlines (message state))
+    print (i_am_at state)
+
+help state = state { message = describeHelp }
 introduction = printLines describeIntroduction
 
 readCommand :: IO String
@@ -75,19 +80,24 @@ readCommand = do
 
 gameLoop :: State -> IO State
 gameLoop state = do
-    print state
+    printState state
+    let newState = state { message = [""] }
+    printLines ["", "Waiting for command:"]
     cmd <- readCommand
-    gameLoop (case cmd of
-        -- "help" -> help state
-        "w" -> go state North
-        "d" -> go state East
-        "a" -> go state West
-        "s" -> go state South
-        _ -> state
-        )
+    if not (cmd == "quit") then
+        gameLoop (case cmd of
+            "help" -> help newState
+            "w" -> go newState North
+            "d" -> go newState East
+            "a" -> go newState West
+            "s" -> go newState South
+            _ -> newState
+            )
+        else do 
+            printLines ["Goodbye"]
+            return(newState)
 
 main :: IO State
 main = do
     introduction
-    -- help (State "room_of_thomas_and_giulia")
-    gameLoop (State "room_of_thomas_and_giulia")
+    gameLoop (help (State [""] "room_of_thomas_and_giulia"))
