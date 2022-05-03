7b6f8713ccf87d152896542c149f621c4c81fdff
Ksawery Chodyniecki <ksawery.chodyniecki@gmail.com>
2022-05-03 09:37:20 +0200

    added makefiles

--- src/hs/main.hs
+++ src/hs/main.hs
@@ -1,5 +1,13 @@
 -- TheAlpineTwist, by Ksawery Chodyniecki, Karolina Romanowska and Grzegorz Rusinek.
 
+import Prelude hiding (lookup)
+import qualified Data.Map.Strict as Map
+
+data Direction = North | West | East | South
+    deriving (Read, Show, Enum)
+
+paths = Map.fromList [("room_of_thomas_and_giulia", "w")]
+
 describeIntroduction = [
     "Daily life in the alpine hotel was disrupted by the barking of the Promyczek. The wife, which came after that to the room, saw her husband on the floor. She started screaming and woke up everyone who stayed in the hotel. The hotel owner immediately calls you to solve that riddle. Who is the murderer? You don't know. Yet..."
     ]
