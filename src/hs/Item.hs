b7e192c8d90e3648358f3125df28eeea3b32faf7
Grzegorz Rusinek <kszesiek@gmail.com>
2022-05-23 11:48:37 -0700

    setting, adding, subtracting amounts of things

--- src/hs/Item.hs
+++ src/hs/Item.hs
@@ -24,7 +24,28 @@ module Item where
     inventory state = state { message = "You have these items: " : map (`describeItem` state) (holding state) }
 
     describeItem :: String -> State -> [Char]
-    describeItem item state = if item `elem` Prelude.map fst (amount state) then
+    describeItem item state = if item `elem` map fst (amount state) then
             item ++ " x" ++ show (findWithDefault 1 item (fromList (amount state)))
         else
             item
+    
+    setAmount state item newAmount =
+        if item `elem` holding state then
+            if item `elem` map fst (amount state) then
+                state { amount = map (\x -> if fst x == item then (item, newAmount) else x) (amount state), message = ["Amount of " ++ item ++ " changed!"] }
+            else
+                state { amount = amount state ++ [(item, newAmount)], message = ["Amount of " ++ item ++ " changed!"] }
+        else state { message = ["You can't change the amount of an item you don't have."] }
+
+    addAmount state item addAmnt = 
+        if item `elem` map fst (amount state) then
+            state { amount = map (\x -> if fst x == item then (item, snd x + addAmnt) else x) (amount state), message = ["Amount of " ++ item ++ " increased!"] }
+        else state { message = ["You can't change the amount of an item that doesn't have an amount."] }
+
+    subtractAmount state item subAmnt = 
+        if item `elem` map fst (amount state) then
+            if snd (head (filter (\x -> fst x == item) (amount state))) < subAmnt then
+                state { message = ["You don't have enough " ++ item ++ " to subtract that much."] }
+            else
+                state { amount = map (\x -> if fst x == item then (item, snd x - subAmnt) else x) (amount state), message = ["Amount of " ++ item ++ " decreased!"] }
+        else state { message = ["You can't change the amount of an item that doesn't have an amount."] }
